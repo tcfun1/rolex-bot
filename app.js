48410a4276933a17a3811c943cfa25b08e14b571
TheBaconnator <Greenboy.NB@gmail.com>
2017-12-20 11:28:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -120,6 +120,9 @@ client.on("message", async message => {
   if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
     
+    if(!message.member.roles.some(r=>["God", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
     // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
     
