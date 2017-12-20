00962061a031d43f042e72ef4e4015960a703e95
TheBaconnator <Greenboy.NB@gmail.com>
2017-12-20 10:27:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
-//Prefix
-const prefix = ",";
-
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
