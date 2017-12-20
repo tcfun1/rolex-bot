50386f30396788cfd5fdfa21cd8e000bdf1484c8
TheBaconnator <Greenboy.NB@gmail.com>
2017-12-20 12:24:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -130,11 +130,9 @@ client.on("message", async message => {
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
-    if (message.channel.type == 'text') {
-        message.channel.fetchMessages()
-          .then(messages => {
-            message.channel.bulkDelete(messages);
-            messagesDeleted = messages.array().length; // number of messages deleted
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 });
