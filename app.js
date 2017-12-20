dae1375f7f283c7018d94f918776d02a4c48ee3b
TheBaconnator <Greenboy.NB@gmail.com>
2017-12-20 11:01:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -128,12 +128,12 @@ client.on("message", async message => {
       return message.reply("Sorry, you don't have permissions to use this!");
     
     // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    if(!deleteCount || deleteCount < 1 || deleteCount > 100)
+      return message.reply("Please provide a number between 1 and 100 for the number of messages to delete");
     
     // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.Delete(fetched)
+    const fetched = await message.channel.fetchMessages({deleteCount});
+    message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 });
