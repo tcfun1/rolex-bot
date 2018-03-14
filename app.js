741d1ba4390a2759b23cd77330f3e6a54644a66c
TheBaconnator <Greenboy.NB@gmail.com>
2018-03-14 11:50:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -45,13 +45,6 @@ client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
   client.user.setGame(`on ${client.guilds.size} servers`);
-  
-});
-bot.on("ready", () => {
-bot.user.setStatus("Online")
-bot.user.setGame("Test")
-bot.user.setGame("Test", "https://www.twitch.tv/therealbaconnator")
-});
 
 client.on("message", async message => {
   // This event will run on every single message received, from any channel or DM.
