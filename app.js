bf6c8ee2f83dc7e2ffc88ce1377e0a2ff36c3653
TheBaconnator <Greenboy.NB@gmail.com>
2018-03-14 12:37:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -66,9 +66,28 @@ client.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
+   client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === 'join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
+  
   // Let's go with a few common example commands! Feel free to delete or change those.
- 
-  if (command === "avatar") {
+
+    if (command === "avatar") {
     // Send the user's avatar URL
     message.channel.send(message.author.avatarURL);
   }
