f1d685b02842a892ce8fcca551d134dc8f01e8b8
TheBaconnator <Greenboy.NB@gmail.com>
2018-03-14 14:09:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -66,50 +66,6 @@ client.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-  const Discord = require('discord.js');
-  const client = new Discord.Client();
-
-client.login('NDE4NDI5MTAxMTU0NTAwNjA4.DYri9A.pwxscCmBr82KDzp-h3yxnRUweRo');
-  
-   client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === ',join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
-  
-  dispatcher.on('end', () => {
-  // The song has finished
-});
-
-dispatcher.on('error', e => {
-  // Catch any errors that may arise
-  console.log(e);
-});
-
-dispatcher.setVolume(0.5); // Set the volume to 50%
-dispatcher.setVolume(1); // Set the volume back to 100%
-
-console.log(dispatcher.time); // The time in milliseconds that the stream dispatcher has been playing for
-
-dispatcher.pause(); // Pause the stream
-dispatcher.resume(); // Carry on playing
-
-dispatcher.end(); // End the dispatcher, emits 'end' event
-  // Let's go with a few common example commands! Feel free to delete or change those.
-
     if (command === "avatar") {
     // Send the user's avatar URL
     message.channel.send(message.author.avatarURL);
